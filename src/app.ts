ef07fac6ae38b3035de0395a803ad4474a30fae9
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2022-01-02 17:13:09 +0200

    first commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, {Request, Response, NextFunction} from "express"
+import {json, urlencoded} from "body-parser";
+
+const app = express()
+
+app.use(json())
+
+app.use(urlencoded({extended: true}))
+
+// Default error handling
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(400).json({message: "Something went wrong!"})
+})
+
+app.listen(3000)
